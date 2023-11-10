11eca910a7a52470d911baa9c6cfdf64d24e8c4c
Frivox1 <mertens.valery@gmail.com>
2023-11-10 21:32:28 +0100

    setup de la db avec fonction pour créer un utilisateur

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 const path = require("path");
 const express = require("express");
+const { default: mongoose } = require("mongoose");
+const session = require("express-session");
+const crypto = require("crypto");
+
 const app = express();
 
 app
@@ -10,9 +14,18 @@ app
     path.join(__dirname, "..", "public")
   ))
   .use("/", require("./routes/index"))
-  .use("/incidents", require("./routes/incident"));
-
+  .use("/incidents", require("./routes/incident"))
+  .use(session({
+    secret : "secret",
+  }));
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("Listening at http://localhost:8080");
+
+  mongoose.connect("mongodb://localhost:27017/projet-prep", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
 });

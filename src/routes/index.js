b9cfb0815384d7deff043723f5783982af6d3794
Mertens Valéry <73830772+Frivox1@users.noreply.github.com>
2023-11-16 16:33:24 +0100

    Add the error display (#23)
    * Commencement pour l'affichage d'erreur
    
    * Display error for register
    
    * change of the error display
    
    * Display error done
    
    * append what missing
    
    ---------
    
    Co-authored-by: Jocke <jockerider199@gmail.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ const Incident = require('../models/incident');
 router.get("/", async (req, res) => {
 
   let loggedIn = req.session.user ? true : false;
+  let error = req.session.error;
 
   let query = req.query.q;
   let incidents = [];
@@ -30,7 +31,7 @@ router.get("/", async (req, res) => {
       .limit(20);
   }
 
-  res.render("pages/index", { incidents, loggedIn, user: req.session.user, query });
+  res.render("pages/index", { incidents, loggedIn, user: req.session.user, error, query });
 });
 
 router.post("/signup", async (req, res) => {
@@ -47,10 +48,21 @@ router.post("/signup", async (req, res) => {
     email: email,
   });
 
-  // Insert the user data into the database
+  // New code to verifie if the user already exists
+  const userExists = await User.findOne({ username: username });
+
+  if (userExists) {
+    console.log("User already exists");
+    req.session.error = "Nom d'utilisateur déjà existant";
+    return res.redirect('/');
+  }
+  // and end here
+
   await user.save();
 
   req.session.user = { name: user.name };
+
+  req.session.error = null;
 
   res.redirect("/");
 });
@@ -63,6 +75,7 @@ router.post("/login", async (req, res) => {
 
   if (!user) {
     console.log("User not found");
+    req.session.error = "Utilisateur non trouvé";
     return res.redirect('/');
   }
 
@@ -72,13 +85,19 @@ router.post("/login", async (req, res) => {
   const hashedPassword = hash.digest('hex');
 
   // Compare the hashed password with the stored password
-  if (hashedPassword !== user.password) {
+
+  const incorrect = hashedPassword !== user.password;
+
+  if (incorrect) {
     console.log("Invalid password");
+    req.session.error = "Mot de passe incorrect";
     return res.redirect('/');
   }
 
   // If the password is correct, log the user in
   req.session.user = { name: user.name };
+
+  req.session.error = null;
 
   res.redirect('/');
 });

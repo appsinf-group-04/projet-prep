053065413217cf6b6aa2c5317a6a007dd5869704
NotAJocke <jockerider199@gmail.com>
2023-11-16 09:50:27 +0100

    feat: search logic

--- src/models/incident.js
+++ src/models/incident.js
@@ -1,14 +1,14 @@
-const {model, Schema} = require('mongoose');
+const { model, Schema } = require('mongoose');
 
 const incidentSchema = new Schema({
-    title: String,
-    description: String,
-    address: String,
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    name: String
+  title: String,
+  description: String,
+  address: String,
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  name: String
 });
 
 module.exports = model('Incident', incidentSchema);
